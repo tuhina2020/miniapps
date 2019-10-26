d26a3d047cd84390e8a9e583ec853557ca405b14
Tuhina Chatterjee <tuhina@sharechat.co>
2019-10-26 12:30:18 +0530

    diwali webcard working

--- src/diwali/input-container/index.js
+++ src/diwali/input-container/index.js
@@ -1,9 +1,40 @@
 import styles from './index.css';
 import * as utils from '@/utils';
+import whatsapp from '@/diwali/assets/whatsapp.png';
+import _get from 'lodash/get';
+import _each from 'lodash/each';
 
 export default class InputContainer {
     constructor(props) {
         this.state = props;
+    }
+
+    imageToBackground(hide) {
+        this.state.hide = hide;
+        _each(['background-video', 'diwali-text-container'], (id) => {
+            const ele1 = document.getElementById(id);
+            if(hide && ele1) {
+                ele1.classList.add("hide-video");
+            } else if(ele1) {
+                ele1.classList.remove("hide-video");
+            }
+        });
+        this.toggleVisibility({ hide , id : 'loading-wrapper' });
+        const input = document.getElementById('input-username');
+        if (hide && input) {
+            input.setAttribute('disabled', true);
+        } else if (input) {
+            input.removeAttribute('disabled');
+        }
+    }
+
+    toggleVisibility({ hide , id }) {
+        const ele = document.getElementById(id);
+        if(hide && ele) {
+            ele.classList.remove('hide-loader')
+        } else if (ele) {
+            ele.classList.add("hide-loader");
+        }
     }
 
     render() {
@@ -24,15 +55,24 @@ export default class InputContainer {
             }
         });
 
+        const w = utils.createNewDiv({
+            type: 'img',
+            setAttribute: {
+                class: 'whatsapp',
+                src: whatsapp
+            }
+        })
+
         const button = utils.createNewDiv({
-            type: 'button',
+            type: 'div',
             setAttribute: {
                 id: 'submit',
+                class: 'hide-loader'
             }
         });
 
         button.innerText = 'Share';
-
+        button.appendChild(w);
         wrapper.appendChild(input);
         wrapper.appendChild(button);
         return wrapper;
@@ -48,14 +88,16 @@ export default class InputContainer {
         const overlay1 = utils.createNewDiv({
             type: 'div',
             setAttribute: {
-              class: 'overlay1'
+              class: 'overlay1',
+              id: 'overlay1'
             }
           });
         overlay1.innerText = name;
         const overlay2 = utils.createNewDiv({
             type: 'div',
             setAttribute: {
-                class: 'overlay2'
+                class: 'overlay2',
+                id: 'overlay2'
             }
         });
         overlay2.innerText = 'Wishes';
@@ -72,32 +114,150 @@ export default class InputContainer {
 
     events() {
         const input = document.getElementById('input-wrapper');
-        const self = this;
         const changeHandler = (e) => {
-            const { target: { value } } = e;
-            self.state.name = value;
+            const { target: { value }, keyCode } = e;
+            let val;
+            this.toggleVisibility({ hide : false , id : 'error-text' });
+            this.sentMessage( '','error-text')
+            if(value.length > 12) val = value.slice(0,12);
+            if(keyCode === 13) {
+                return this.getGifByName()
+                .catch(err => console.log(err));
+                // self.createPost()
+                // .then(this.whatsappShare)
+            }
+            this.state.name = (val || value);
             this.removeTextOverlay();
             this.addTextOverlay();
-            console.log('THIS IS IT : ', self.state);
-        };
+        }
         input.addEventListener('keyup', changeHandler);
-
         const button = document.getElementById('submit');
-        const submit = (e) => self.getGifByName();
+        const submit = (e) => {
+            this.getGifByName()
+              .catch(err => console.log(err));
+            // self.createPost()
+            // .then(this.whatsappShare)
+        };
         button.addEventListener('click', submit);
+    }
+
+    sentMessage(text, id='overlay2') {
+        const ele = document.getElementById(id);
+        ele.innerText = text;
     }
 
     getGifByName() {
         const { name, Authorization } = this.state;
-        const requestObj = {
-            url: 'http://127.0.0.1:3000/generateGif',
-            method: 'POST',
-            headers: { Authorization },
-            body: { name }
+        console.log('NAME ', name)
+        if(!name || (name && name.length === 0)) {
+            this.toggleVisibility({ hide : true , id : 'error-text' });
+            this.sentMessage( 'Please Enter Your name','error-text');
+            return Promise.resolve();
+        } else {
+            const requestObj = {
+                url: 'https://apis.sharechat.com/festive-webcard-service/generateVideo',
+                method: 'POST',
+                headers: { Authorization },
+                body: {
+                    "festivalName": "Diwali",
+                    "wisherName": name
+                }
+            };
+            this.imageToBackground(true);
+            return utils
+                .request(requestObj)
+                // .then((data) => this.createPost({ data, Authorization }))
+                // .then(data => { this.sentMessage(JSON.stringify(data)); return data; })
+                .then((data) => { this.imageToBackground(false); return data; })
+                .then((data) => { this.resetText();  return data; })
+                .then(this.whatsappShare)
+                .catch((err) => { this.imageToBackground(false); })
+        }
+    }
+
+    whatsappShare(data) {
+        // console.log(data, 'WHATSAPP SHARE')
+        const postId = _get(data, 'PostDetails.postId', 595856933);
+        const json = {
+            type : "shareWebCard",
+            postId: postId.toString()
         };
+
+        console.log(json);
+        Android.onAction(JSON.stringify(json));
+    }
+
+    resetText() {
+        console.log('RESET ')
+        const input = document.getElementById('input-username');
+        input.value = null;
+        this.state.name = null;
+        document.getElementById('overlay1') && document.getElementById('overlay1').remove();
+        document.getElementById('overlay2') && document.getElementById('overlay2').remove();
+    }
+
+    createPost({ data, Authorization }) {
+        const { videoDetails: { fileUrl } } = data;
+        const requestObj = {
+            url : "https://restapi1.sharechat.com/requestType47",
+             method: 'POST',
+             headers: { /*Authorization*/ },
+             body: {
+                "bn": "broker1",
+                "client": "android",
+                "deviceId": "8635fe6be2f187fd",
+                "message": {
+                  "webcard": true,
+                  "a": "288330914",
+                  "m": "Hindi",
+                  "y": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Frangipani_flowers.jpg/800px-Frangipani_flowers.jpg",
+                  "c": "##diwali#",
+                  "captionTagsList": [
+                    {
+                      "bucketId": "11",
+                      "bucketName": "शुभकामनाएं एवं सुविचार",
+                      "isAdult": false,
+                      "tagId": "845755",
+                      "tagName": "#diwali#"
+                    }
+                  ],
+                  "cd": 0,
+                  "create_type": "File Manager",
+                  "d": 0,
+                  "encodedTextV2": "{[{845755}]} ",
+                //   "e": fileUrl,
+                  "liveStatus": false,
+                  "q": "video/mp4",
+                  "optionsPoll": [],
+                  "pi": Date.now(),
+                  "postCreationLatLong": "12.936936936936936,77.60959875038247",
+                  "postCreationLocation": "bangalore urban, KA",
+                  "h": 360,
+                  "z": 2073254,
+                  "t": "video",
+                  "w": 360,
+                  "o": Date.now(),
+                  "sd": 0,
+                  "ad": 1,
+                  "createdVia": "web_card_diwali",
+                  "tt": [
+                    {
+                      "i": "845755",
+                      "n": "#diwali#"
+                    }
+                  ],
+                  "i": "-1571858786412",
+                  "b": "https://cdn.sharechat.com/399a61d3-1df3-4f10-b466-af970f81fc29-dce5d96c-e9ba-40cd-8c1e-f0eca6de11ef_thumb.jpg",
+                  "urlList": [],
+                  "n": "-",
+                  "v": fileUrl
+                },
+                "passCode": "1573236412398651",
+                "resTopic": "response/user_288330914_3aff1f244aecde3eaa86",
+                "userId": "288330914"
+               }
+        }
         return utils
             .request(requestObj)
-            .then(v => console.log(v))
-            .catch(err => console.log(err));
     }
 }
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,11 @@
 export const request = ({ url, headers, method = "GET", body }) => {
+  console.log('HEADERS : ', headers)
   headers = Object.assign(headers, { "content-type": "application/json" });
   return fetch(url, {
     method,
     headers,
-    body: body ? JSON.stringify(body) : undefined
+    body: body ? JSON.stringify(body) : undefined,
+    // mode: 'cors'
   }).then(response => response.json());
 };
 
@@ -42,7 +44,7 @@ export const getAppVersion = () => {
   try {
     appVersion = Android.get("appVersion");
   } catch (e) {
-    appVersion = 10;
+    appVersion = 5000;
   }
   return appVersion;
 };
@@ -58,4 +60,43 @@ export const addOrUpdateUrlParam = (name, value) => {
       window.location.href = href + "&" + name + "=" + value;
     else window.location.href = href + "?" + name + "=" + value;
   }
+};
+
+export const APP_UPDATE_MESSAGES =   {
+  "f": "update_popup_title",
+  "English": "Update your ShareChat now!",
+  "Default": "Update your ShareChat now!",
+  "Hindi": "अपना शेयरचैट अपडेट करें!",
+  "Hindi_US": "Apna ShareChat update karein",
+  "Bhojpuri": "आपन शेयर चैट के अपडेट करीं",
+  "Bhojpuri_US": "Apan sharechat ke update karin",
+  "Haryanvi": "अपणी शेयरचैट अपडेट कर ले",
+  "Haryanvi_US": "Aapni ShareChat update kar le",
+  "Rajasthani": "नयो ShareChat अपडेट करो!",
+  "Rajasthani_US": "Nayo ShareChat update karo!",
+  "Marathi": "नवीन शेअरचॅट अपडेट करा",
+  "Marathi_US": "Navin ShareChat update kara",
+  "Telugu": "మీ షేర్‌చాట్ యాప్‌ని అప్‌డేట్ చేసుకోండి!",
+  "Telugu_US": "Mi sharechat app ni update chesukondi!",
+  "Malayalam": "ഉടന്‍ തന്നെ നിങ്ങളുടെ ഷെയര്‍ചാറ്റ് അപ്ഡേറ്റ് ചെയ്യൂ...",
+  "Malayalam_US": "Udan thanne ningalude Sharechat update cheyyoo...",
+  "Gujarati": "તમારું શેરચેટ અપડેટ કરો!",
+  "Gujarati_US": "Tamaru ShareChat update karo",
+  "Punjabi": "ਨਵੇਂ ਨਵੇਂ ਫੀਚਰਸ ਲਈ ਹੁਣੀ ਅਪਡੇਟ ਕਰੋ ਸ਼ੇਅਰਚੈਟ",
+  "Punjabi_US": "Nwen Nwen Features lai huni update kro ShareChat",
+  "Tamil": "இப்போது உங்கள் ஷேர்சாட்டை அப்டேட் பண்ணுங்க!",
+  "Tamil_US": "Eppothu ungal sharechat-ai puthipikkavum",
+  "Bengali": "এক্ষুনি আপনার শেয়ারচ্যাট আপডেট করুন !",
+  "Bengali_US": "Ekhhuni apnar sharechat update korun!",
+  "Kannada": "ಈಗಲೇ ಶೇರ್ ಚಾಟ್ ಅಪ್ಡೇಟ್ ಮಾಡಿ!",
+  "Kannada_US": "Egale ShareChat update maadi!",
+  "Odia": "ନିଜ ଶେୟରଚେଟ୍ କୁ ବର୍ତ୍ତମାନ ଅପଡେଟ କରନ୍ତୁ!",
+  "Odia_US": "Nija ShareChat ku bartamana update karantu!",
+  "Bengali (new)": "এক্ষুনি আপনার শেয়ারচ্যাট আপডেট করুন !",
+  "Assamese": "আপোনাৰ শ্বেয়াৰচাট এতিয়া আপডেট কৰক",
+  "Assamese_US": "Aapunaar share chat etia aapdat karak",
+  "Bangladeshi": "এহনি  আপনার শেয়ারচ্যাট আপডেট করেন !",
+  "Bangladeshi_US": "ahoni apnar sharechat update karen!",
+  "Urdu": "اپنا شیئر چیٹ اپ ڈیٹ کریں",
+  "Urdu_US": "Apna ShareChat update karein"
 };
